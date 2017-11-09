ccb097bbb0473426ef3abc24a10ee23d53d96a64
Jay <jlaura@asu.edu>
2017-11-08 21:43:00 -0700

    Adding in conda3 hash to package name

--- recipes/conda_upload.sh
+++ recipes/conda_upload.sh
@@ -7,5 +7,5 @@ conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 export VERSION='3.0.1'
 conda build .
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER $CONDA_BLD_PATH/$OS/$PKG_NAME-3.0.1-0.tar.bz2 --force
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER $CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION-h$PKG_HASH_0.tar.bz2 --force
 
