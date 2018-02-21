62f475f0ec4eb2e9ce0eb208c65ed73a6233e36a
jay <jlaura@asu.edu>
2018-02-21 13:12:11 -0700

    GCC-6?

--- recipes/build.sh
+++ recipes/build.sh
@@ -4,10 +4,10 @@ set -e # Abort on error
 
 if [[ $(uname) == 'Linux' ]]; then
     if [[ $ARCH -eq 64 ]]; then
-        make -f Makefile.linux64 all install INSTDIR=$PREFIX
+        CC+gcc-6 CXX=g++-6 make -f Makefile.linux64 all install INSTDIR=$PREFIX;
     else
-        make -f Makefile.linux all install INSTDIR=$PREFIX
+        CC+gcc-6 CXX=g++-6 make -f Makefile.linux all install INSTDIR=$PREFIX;
     fi
 else
-    make -f Makefile.mac all install INSTDIR=$PREFIX
+    make -f Makefile.mac all install INSTDIR=$PREFIX;
 fi
