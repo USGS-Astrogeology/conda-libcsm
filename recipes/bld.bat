68daa647e28d812df6bb7aa089043c215fc6b562
jay <jlaura@usgs.gov>
2018-10-16 08:19:46 -0700

    Updates build to use cmake

--- recipes/bld.bat
+++ recipes/bld.bat
@@ -1,8 +1,10 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-cl.exe /D_USRDLL /D_WINDLL /EHsc *.cpp /link /DLL /OUT:csmapi.dll
-copy csmapi.lib %LIBRARY_LIB%
+mkdir build
+cd build
+cmake ..
+cmake --build . --target ALL_BUILD --config Release
+copy Release\csmapi.lib %LIBRARY_LIB%
 mkdir %LIBRARY_INC%\csm\
-copy *.h %LIBRARY_INC%\csm\
-copy csmapi.dll %LIBRARY_BIN%
+copy Release\*.h %LIBRARY_INC%\csm\
+copy Release\csmapi.dll %LIBRARY_BIN%
 
 if errorlevel 1 exit 1 
