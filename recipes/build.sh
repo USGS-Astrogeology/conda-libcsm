c80d988690fa5b88170741d0d4d2276cf9a8b862
jay <jlaura@usgs.gov>
2018-10-16 08:20:41 -0700

    Removes explicit compiler

--- recipes/build.sh
+++ recipes/build.sh
@@ -4,9 +4,9 @@ set -e # Abort on error
 
 if [[ $(uname) == 'Linux' ]]; then
     if [[ $ARCH -eq 64 ]]; then
-        CC=gcc-6 CXX=g++-6 make -f Makefile.linux64 all install INSTDIR=$PREFIX;
+        make -f Makefile.linux64 all install INSTDIR=$PREFIX;
     else
-        CC=gcc-6 CXX=g++-6 make -f Makefile.linux all install INSTDIR=$PREFIX;
+        make -f Makefile.linux all install INSTDIR=$PREFIX;
     fi
 else
     make -f Makefile.mac all install INSTDIR=$PREFIX;
