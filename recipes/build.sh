56eeebfe628c1af2e98c39048400d95730bd46a9
jlaura <jlaura@usgs.gov>
2018-10-22 06:55:52 -0700

    Update build.sh

--- recipes/build.sh
+++ recipes/build.sh
@@ -2,14 +2,6 @@
 
 set -e # Abort on error
 
-if [[ $(uname) == 'Linux' ]]; then
-    if [[ $ARCH -eq 64 ]]; then
-        make -f Makefile.linux64 all install INSTDIR=$PREFIX CC=$CC CXX=$CXX;
-    else
-        make -f Makefile.linux all install INSTDIR=$PREFIX CC=$CC CXX=$CXX;
-    fi
-else
-    make -f Makefile.mac all install INSTDIR=$PREFIX;
-fi
-
-
+mkdir build && cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX ..
+make install
