4e12ed636676f6c7ade81cb94a32be90d0b9eeb0
jlaura <jlaura@usgs.gov>
2019-01-05 19:41:16 -0700

    Update bld.bat

--- recipes/bld.bat
+++ recipes/bld.bat
@@ -4,7 +4,7 @@ cmake ..
 cmake --build . --target ALL_BUILD --config Release
 copy Release\csmapi.lib %LIBRARY_LIB%
 mkdir %LIBRARY_INC%\csm\
-dir
+dir Release\
 copy Release\*.h %LIBRARY_INC%\csm\
 copy Release\csmapi.dll %LIBRARY_BIN%
 
