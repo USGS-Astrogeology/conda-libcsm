9096a434cb01d81a6e3e4209f20ae41f611ce80e
Jay <jlaura@usgs.gov>
2019-01-06 12:46:01 -0700

    Trying to get win builds working

--- recipes/bld.bat
+++ recipes/bld.bat
@@ -2,11 +2,9 @@ mkdir build
 cd build
 cmake ..
 cmake --build . --target ALL_BUILD --config Release
-dir Release
-dir Win32
 copy Release\csmapi.lib %LIBRARY_LIB%
 mkdir %LIBRARY_INC%\csm\
-copy Release\*.h %LIBRARY_INC%\csm\
+copy ..\*.h %LIBRARY_INC%\csm\
 copy Release\csmapi.dll %LIBRARY_BIN%
 
 if errorlevel 1 exit 1 
